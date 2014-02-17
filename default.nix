ffc2986eff1a9a0493d309534442a9215d737e14
Oliver Charles <ollie@ocharles.org.uk>
2014-02-17 12:53:52 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,9 @@
-{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
-let
-  pkgs = import <nixpkgs> {};
-
-  inherit (haskellPackages) async blazeBuilder cabal cabalInstall_1_18_0_2 extensibleEffects snap websocketsSnap pipes pipesConcurrency;
-
-in cabal.mkDerivation (self: {
+{ cabal, async, blazeBuilder, extensibleEffects, snap, websocketsSnap, pipes
+, pipesConcurrency
+}:
+cabal.mkDerivation (self: {
   pname = "snaplet-socketio";
   version = "1.0.0";
   src = ./.;
   buildDepends = [ async blazeBuilder extensibleEffects snap websocketsSnap pipes pipesConcurrency ];
-  buildTools = [ cabalInstall_1_18_0_2 pkgs.nodejs ];
 })
